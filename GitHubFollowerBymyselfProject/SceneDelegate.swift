2462cc9e66eb2dde27790d306cc215f9efe814ff
hrKim0303 <85341050+umaKim@users.noreply.github.com>
2023-09-04 01:30:35 +0900

    prevent memory release

--- GitHubFollowerBymyselfProject/SceneDelegate.swift
+++ GitHubFollowerBymyselfProject/SceneDelegate.swift
@@ -11,6 +11,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     
+    var appContainer: AppContainerImplementation!
+    var appCoordinator: AppCoordinator!
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -18,9 +21,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.windowScene   = windowScene
         
         guard let window      = window else { return }
-        let appContainer      = AppContainerImplementation()
-        let appCoordinator    = AppCoordinator(window: window, container: appContainer)
+        appContainer          = AppContainerImplementation()
+        appCoordinator        = AppCoordinator(window: window, container: appContainer)
         appCoordinator.start()
     }
 }
-
